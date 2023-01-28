53a0a631660a858cdf1158b4eea34b1b9dae7429
Nathan Laan <nathan@laan.pro>
2023-01-28 12:05:28 -0700

    fix: Additional error handling and running checks.

--- src/background.js
+++ src/background.js
@@ -6,38 +6,43 @@ function clog(m) {
 let running = false;
 let intervalFunction;
 const wdCookieURL = "https://wd5.myworkday.com";
-const sleepTime = 30000;
+const sleepTime = 1000;
 
 function incrementCookie(details, cookieValue) {
   details.value = (parseInt(cookieValue) + sleepTime).toString();
   chrome.cookies.set(details)
-  .then(cookie => clog(details.name + " -> " + cookie.value))
-  .catch(error => console.log(error));
+    .then(cookie => clog("Update: " + details.name + " -> " + cookie.value))
+    .catch(error => console.log(error));
 }
 
 function preventWorkdayTimeout() {
-  const detailsLUA = { url: wdCookieURL, name: "LastUserActivity" };
-  const detailsSTM = { url: wdCookieURL, name: "SessionTimeoutMS" };
-  chrome.cookies.get(detailsLUA)
-    .then(cookie => incrementCookie(detailsLUA, cookie.value))
-    .catch(error => console.log(error));
-  chrome.cookies.get(detailsSTM)
-    .then(cookie => incrementCookie(detailsSTM, cookie.value))
-    .catch(error => console.log(error));
+  if(running) {
+    //
+    // Scoped here, not globally, because incrementCookie() adds 'value' field
+    //
+    const detailsLUA = { url: wdCookieURL, name: "LastUserActivity" };
+    const detailsSTM = { url: wdCookieURL, name: "SessionTimeoutMS" };
+    chrome.cookies.get(detailsLUA)
+      .then(cookie => incrementCookie(detailsLUA, cookie.value))
+      .catch(error => console.log(error));
+    chrome.cookies.get(detailsSTM)
+      .then(cookie => incrementCookie(detailsSTM, cookie.value))
+      .catch(error => console.log(error));
+  }
   return true;
 }
 
 chrome.action.onClicked.addListener(function (tab) {
   try {
-    if(running){
-      running = !running;
+    if(running) {
+      running = false;
       clog("Stopping Prevent-Timeout-Workday.");
       clearInterval(intervalFunction);
       chrome.action.setTitle({ title: "Prevent-Workday-Timeout STOPPED" });
       chrome.action.setIcon({ path: "/images/extension-icon-stopped-128.png" })
         .catch(error => console.log(error));
     } else {
-      running = !running;
+      running = true;
       clog("Starting Prevent-Timeout-Workday.");
       intervalFunction = setInterval(preventWorkdayTimeout, sleepTime);
       chrome.action.setTitle({ title: "Prevent-Workday-Timeout RUNNING"});
@@ -46,6 +51,8 @@ chrome.action.onClicked.addListener(function (tab) {
     }
   } catch(err) {
     clog(err);
+    clearInterval(intervalFunction);
+    running = false;
   }
   // NOT NEEDED return true;
 });
@@ -53,3 +60,6 @@ chrome.action.onClicked.addListener(function (tab) {
 //
 // TODO: clearInterval on browser close.
 //
+chrome.runtime.onSuspend.addListener(() => {
+  clearInterval(intervalFunction).catch(error => console.log(error));
+});
